62e9f311b3790d37c80532c2efeca271d892a5bb
Barks347 <john.doe92@mail.ru>
2015-06-05 23:17:23 +0300

    Initial commit

---
+++ src/com/barks/Main.java
@@ -0,0 +1,14 @@
+package com.barks;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+    System.currentTimeMillis();
+
+
+
+    }
+}
